eb4f80478930c381cdfdcc782e4d3982ebc7968d
renes <renes@SEBASTIAN>
2024-08-24 13:30:33 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PracticaMetodos;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //primer = int.Parse(Console.ReadLine());
+        //segundo= 
+        Mensaje();
+        SumaNumeros(15, 25);
+    }
+
+    static void Mensaje()
+    {
+        Console.WriteLine("Primer mensaje en pantalla");
+    }
+
+    ////static int Suma(primer, segundo)
+    //{
+
+    //    resultado = primer + segundo;
+    //    return resultado;
+    //}
+
+    static void SumaNumeros(int num1, int num2)
+    {
+        var resultado = num1 + num2;
+        //La siguiente nomencaltura sirve para poder poner strings inteligentes ($"{}")
+        Console.WriteLine($"la suma de los numeros es {resultado}");
+
+    }
+
+}
