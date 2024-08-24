ed9da76cf0bab8f78fd8ff6508b20ff6eb2e31a0
renesebastianJoo <renesebastian700@gmail.com>
2024-08-24 15:54:32 -0600

    Se añaden la sobrecarga de metodos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace PracticaMetodos;
 
-internal class Program
+ class Program
 {
+   
     static void Main(string[] args)
     {
         //primer = int.Parse(Console.ReadLine());
@@ -9,19 +10,17 @@ internal class Program
         Mensaje();
         SumaNumeros(15, 25);
         SumaTresNumeros(10,20,30);
+        Divide2Numeros(3, 5);
+        Console.WriteLine(Multiplicacion(3, 5));
+        Console.WriteLine(Multiplicacion(15.5, 23.7));
+        Console.WriteLine(Multiplicacion(3, 5, 7));
+
     }
 
     static void Mensaje()
     {
         Console.WriteLine("Primer mensaje en pantalla");
     }
-
-    ////static int Suma(primer, segundo)
-    //{
-
-    //    resultado = primer + segundo;
-    //    return resultado;
-    //}
 
     static void SumaNumeros(int num1, int num2)
     {
@@ -38,5 +37,19 @@ internal class Program
         Console.WriteLine($"El resultado de la suma de los tres nuemros es {res}");
         return res;
     }
+
+    static double Divide2Numeros(double num1, double num2)
+    {
+        var result = num1 / num2;
+        Console.WriteLine($"el resultado de la divisón es de {result}");
+        return result;
+    }
+    //Sobre carga de metodos
+    static int Multiplicacion(int operador1, int operador2) => operador1 * operador2;
+    static double Multiplicacion(double num1, double num2) => num1 * num2;
+
+    static int Multiplicacion(int op1, int op2, int op3) => op1 * op2 * op3;
+
+
 
 }
--- README.md
+++ README.md
@@ -12,4 +12,16 @@ Genera la suma de dos numeros, como se puede ver este programa no devuelve nada 
 ## SumaTresNumeros
 Genera la suma de tres numeros pero esta vez si se retorna un valor, lo que significa que puede ser utilizado más tarde
 
+## Divide 2 numeros
+Este metodo devuelve la division aqui ha que aclarar algo y es que si las variables fueran int no funcionaria ya que para poder procesar se requeieren 
+que sean de tipo flotante, por lo que se cambia a tipo double
 
+# Sobrecarga de Metodos
+Para hacer una sobrecarga de metodos, o sea tener metodos que tengan el mismo nombre se tiene que 
+hacer una sobrecarga de metodos esto es que los metodos que se llaman igual tengan diferentes tipos
+o que tengan diferente numero de variables en este caso se hace esto con el metodo Multiplicacion el 
+cual cuanta con 2 sobrecargas diferentes.
+
+## Multiplicacion
+Los metodos multiplicacion se hacen usando la funcion fleca para que sean mas legibles.
+Estos son 2 enteros, 2 doubles o 3 enteros para que se entienda la idea
